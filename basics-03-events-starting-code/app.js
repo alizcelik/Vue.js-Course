3708b5af80eef5fcfcdbbc77af10ddf5d9d45b67
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-09-28 17:02:24 +0300

    click event

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,6 +4,18 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add(){
+      if(this.counter < 10){
+        this.counter++;
+      }
+    },
+    subtract(){
+      if(this.counter > 0){
+        this.counter--;
+      }
+    }
+  }
 });
 
 app.mount('#events');
