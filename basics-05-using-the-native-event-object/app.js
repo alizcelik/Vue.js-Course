3a1fff1f5f36eada54cca8c24eb06e68edbdafaf
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-09-29 17:07:16 +0300

    set timout

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -3,25 +3,42 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      //fullName: "",
     };
   },
   watch: {
-    name(value) {
-      if (value === "") {
-        this.fullname = "";
-      } else {
-        this.fullname = value + " " + "Özçelik";
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
       }
     },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
   },
   computed: {
-    // fullname(){
-    //   console.log('Running...');
-    //   if(this.name === ''){
-    //     return '';
-    //   }
-    //   return this.name + ' ' + 'Özçelik';
-    // }
+    fullname() {
+      console.log("Running...");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
   },
   methods: {
     outputFullName() {
