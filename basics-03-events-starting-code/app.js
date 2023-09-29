4d2ccb9c8b753c2bb30c3a927bb8f675bf75d928
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-09-29 12:46:53 +0300

    event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,9 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:'',
     };
   },
   methods: {
+    submitForm(){
+      alert('Submitted!');
+    },
+    setName(event, lastName){
+      this.name = event.target.value + ' ' + lastName;
+    },
     add(num){
       if(this.counter < 9){
         this.counter = this.counter + num;
