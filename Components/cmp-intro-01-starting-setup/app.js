d963d36b9ac9b680a50b07673b9b785974d50c4c
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-11-06 14:42:48 +0300

    component. app

--- Components/cmp-intro-01-starting-setup/app.js
+++ Components/cmp-intro-01-starting-setup/app.js
@@ -1,14 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "01234 5678 991",
-          email: "manual@mail.com",
-        },
         {
           id: "julie",
           name: "Julie Jones",
@@ -16,6 +9,37 @@ const app = Vue.createApp({
           email: "julia@mail.com",
         },
       ],
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.component("friend-contact", {
+  template: `
+     <li >
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails()">
+            {{ detailsAreVisible ? 'Hide' : 'Show'}} Details
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone}}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manual@mail.com",
+      },
     };
   },
   methods: {
