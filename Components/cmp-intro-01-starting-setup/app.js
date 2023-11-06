27a1d4b0c52d0cf9445eb883f1d0cb455d4f3a7e
Ali Özçelik <ali.oezcelik@cosmino.de>
2023-11-06 14:33:08 +0300

    button problem

---
+++ Components/cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manual@mail.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julia@mail.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount("#app");
